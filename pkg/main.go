75b908b48ea6a530aba200f0daddecd112c2c710
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-05 16:35:04 +0530

    updated deps

--- pkg/main.go
+++ pkg/main.go
@@ -2,13 +2,13 @@ package pkg
 
 import (
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/s3website"
+	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/awsstaticwebsite"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/provider/aws/pulumiawsprovider"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 type ResourceStack struct {
-	Input  *s3website.S3WebsiteStackInput
+	Input  *awsstaticwebsite.AwsStaticWebsiteStackInput
 	Labels map[string]string
 }
 
--- pkg/outputs/outputs.go
+++ pkg/outputs/outputs.go
@@ -1,11 +1,11 @@
 package outputs
 
 import (
-	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/s3website"
+	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/awsstaticwebsite"
 	"github.com/pulumi/pulumi/sdk/v3/go/auto"
 )
 
 func PulumiOutputsToStackOutputsConverter(pulumiOutputs auto.OutputMap,
-	input *s3website.S3WebsiteStackInput) *s3website.S3WebsiteStackOutputs {
-	return &s3website.S3WebsiteStackOutputs{}
+	input *awsstaticwebsite.AwsStaticWebsiteStackInput) *awsstaticwebsite.AwsStaticWebsiteStackOutputs {
+	return &awsstaticwebsite.AwsStaticWebsiteStackOutputs{}
 }
